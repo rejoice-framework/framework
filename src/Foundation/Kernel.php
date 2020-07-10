d643c1eaa304ea1451aa4e77dc78490194d2ccf8
CinPer <damien.dorcis@yahoo.com>
2020-07-10 15:36:38 +0000

    Update Kernel.php

--- src/Foundation/Kernel.php
+++ src/Foundation/Kernel.php
@@ -11,18 +11,7 @@
 
 namespace Prinx\Rejoice\Foundation;
 
-require_once __DIR__ . '/../../../../autoload.php';
-require_once __DIR__ . '/../../../dotenv/src/aliases_functions.php';
 require_once __DIR__ . '/../constants.php';
-require_once __DIR__ . '/../Session/FileSession.php';
-require_once __DIR__ . '/../Session/DatabaseSession.php';
-require_once __DIR__ . '/../Menu/Menus.php';
-require_once __DIR__ . '/../Utils/SmsService.php';
-require_once __DIR__ . '/Database.php';
-require_once __DIR__ . '/UserResponse.php';
-require_once __DIR__ . '/Request.php';
-require_once __DIR__ . '/RequestValidator.php';
-require_once __DIR__ . '/Response.php';
 
 use Prinx\Rejoice\Foundation\Database;
 use Prinx\Rejoice\Foundation\FrameworkConfig;
@@ -51,7 +40,7 @@ class Kernel
      *
      * @var string
      */
-    protected $appName = 'default';
+    protected $appName = '';
 
     /**
      * Contains all the defined databases connetions
@@ -283,7 +272,7 @@ class Kernel
      */
     protected $userSavedResponse = null;
 
-    public function __construct($appName)
+    public function __construct($appName = '')
     {
         $this->appName = $appName;
         $this->logger = new Log;
