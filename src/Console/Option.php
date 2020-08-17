cb12f8b9d32ff51724a9448f4a89fe891298fb5d
CinPer <damien.dorcis@yahoo.com>
2020-08-17 13:21:39 +0000

    Create Console Option class

---
+++ src/Console/Option.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Prinx\Rejoice\Console;
+
+use Symfony\Component\Console\Input\InputOption;
+
+class Option extends InputOption
+{
+    const IS_ARRAY = InputOption::VALUE_IS_ARRAY;
+
+    const NONE = InputOption::VALUE_NONE;
+
+    const OPTIONAL = InputOption::VALUE_OPTIONAL;
+
+    const REQUIRED = InputOption::VALUE_REQUIRED;
+}
