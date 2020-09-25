15531d31d99b23a6d9e46a53b5262e163524f6ac
CinPer <damien.dorcis@yahoo.com>
2020-09-25 20:59:23 +0000

    Fix retrieving network giving number

--- src/Foundation/Kernel.php
+++ src/Foundation/Kernel.php
@@ -307,12 +307,12 @@ class Kernel
      *
      * Returns the config object instance if no parameter passed
      *
-     * @param string $key
-     * @param mixed  $default The default to return if the configuration is not found
-     * @param bool   $silent  If true, will shutdown the exception throwing if configuration variable not found and no default was passed.
      *
+     *
+     * @param  string               $key
+     * @param  mixed                $default The default to return if the configuration is not found
+     * @param  bool                 $silent  If true, will shutdown the exception throwing if configuration variable not found and no default was passed.
      * @throws \RuntimeException
-     *
      * @return Config|mixed
      */
     public function config($key = null, $default = null)
@@ -393,7 +393,7 @@ class Kernel
     public function attemptsToCallSubMenuDirectly()
     {
         return !$this->isFirstRequest() &&
-            $this->session->isNew();
+        $this->session->isNew();
     }
 
     public function doesNotAllowDirectSubMenuCall()
@@ -540,15 +540,15 @@ class Kernel
      *
      * @todo Search a proper way of determining if moving to next menu
      *
-     * @param string $nextMenu
      *
+     * @param  string $nextMenu
      * @return bool
      */
     public function isMovingToMenu($nextMenu)
     {
         return $nextMenu === APP_END ||
-            $nextMenu === APP_WELCOME ||
-            !in_array($nextMenu, RESERVED_MENU_IDs);
+        $nextMenu === APP_WELCOME ||
+        !in_array($nextMenu, RESERVED_MENU_IDs);
     }
 
     /**
@@ -560,26 +560,26 @@ class Kernel
      * developer has to specify the validation rules or validate himself in the
      * `validate` method of the menu entity)
      *
-     * @param string $currentMenu
-     * @param string $userError
-     * @param bool   $responseExistsInMenuActions The response has already been specified by the developer
-     * @param string $nextMenu
      *
+     * @param  string  $currentMenu
+     * @param  string  $userError
+     * @param  bool    $responseExistsInMenuActions The response has already been specified by the developer
+     * @param  string  $nextMenu
      * @return bool
      */
     public function mustValidateResponse($userError, $responseExistsInMenuActions, $nextMenu)
     {
         return !$userError &&
-            !$responseExistsInMenuActions &&
-            !in_array($nextMenu, RESERVED_MENU_IDs);
+        !$responseExistsInMenuActions &&
+        !in_array($nextMenu, RESERVED_MENU_IDs);
     }
 
     /**
      * Load the Menu Entity of a particular menu.
      *
-     * @param string $menuName
-     * @param string $entityType ('currentMenuEntity'|'nextMenuEntity')
      *
+     * @param  string $menuName
+     * @param  string $entityType  ('currentMenuEntity'|'nextMenuEntity')
      * @return void
      */
     public function loadMenuEntity($menuName, $entityType)
@@ -595,8 +595,8 @@ class Kernel
     /**
      * Call the proper method to run for the specific next menu.
      *
-     * @param string $nextMenu
      *
+     * @param  string $nextMenu
      * @return void
      */
     protected function runAppropriateState($nextMenu)
@@ -646,8 +646,8 @@ class Kernel
      *
      * From here any subsequent request will be forward to the remote ussd
      *
-     * @param string $nextMenu
      *
+     * @param  string $nextMenu
      * @return void
      */
     protected function switchToRemoteUssd($nextMenu)
@@ -672,8 +672,8 @@ class Kernel
      * If both `save_as` and `saveAs` method are available, the
      * `save_as` parameter has the precedence
      *
-     * @param string $userResponse
      *
+     * @param  string  $userResponse
      * @return void
      */
     protected function saveUserResponse($userResponse)
@@ -1007,8 +1007,8 @@ class Kernel
      * there is placeholders that match each index of the array and we replace
      * each placeholder by its value.
      *
-     * @param string $menuName
      *
+     * @param  string    $menuName
      * @return string
      */
     protected function menuMessage($menuName)
@@ -1054,8 +1054,8 @@ class Kernel
      * the save_as parameter, etc.) in the session (persistMenuActions). and we
      * return only the messages for display purpose.
      *
-     * @param string $menuName
      *
+     * @param  string   $menuName
      * @return array
      */
     protected function menuActions($menuName)
@@ -1352,10 +1352,10 @@ class Kernel
      * in the actionBag. If the parameter replace is true, the old actions will
      * be rather completely replaced by the new actionBag.
      *
-     * @param array  $actionBag
-     * @param bool   $replace
-     * @param string $menuName
      *
+     * @param  array    $actionBag
+     * @param  bool     $replace
+     * @param  string   $menuName
      * @return array
      */
     public function insertMenuActions($actionBag, $replace = false, $menuName = '')
@@ -1384,8 +1384,8 @@ class Kernel
     /**
      * Empty, for this request, the actionBag of a particular menu.
      *
-     * @param string $menuName
      *
+     * @param  string $menuName
      * @return void
      */
     public function emptyActionsOfMenu($menuName)
@@ -1430,8 +1430,8 @@ class Kernel
     /**
      * Returns the previous menu name.
      *
-     * @throws \RuntimeException If nothing is in the history
      *
+     * @throws \RuntimeException If nothing is in the history
      * @return string
      */
     public function previousMenuName()
@@ -1448,9 +1448,9 @@ class Kernel
     /**
      * Allows developer to save a value in the session.
      *
-     * @param string $key
-     * @param mixed  $value
      *
+     * @param  string $key
+     * @param  mixed  $value
      * @return void
      */
     public function sessionSave($key, $value)
@@ -1466,11 +1466,11 @@ class Kernel
      * passed, it throws an exception.
      *
      *
-     * @param string $key
-     * @param mixed  $default
      *
+     *
+     * @param  string     $key
+     * @param  mixed      $default
      * @throws \Exception If the key is not found and no default has been passed
-     *
      * @return mixed
      */
     public function sessionGet($key, $default = null)
@@ -1481,8 +1481,8 @@ class Kernel
     /**
      * Allow developer to check if the session contains an index.
      *
-     * @param string $key
      *
+     * @param  string $key
      * @return bool
      */
     public function sessionHas($key)
@@ -1493,9 +1493,9 @@ class Kernel
     /**
      * Allow the developer to remove a key from the session.
      *
-     * @param string $key
      *
-     * @return bool True if the key exists and has been removed
+     * @param  string $key
+     * @return bool   True if the key exists and has been removed
      */
     public function sessionRemove($key)
     {
@@ -1508,11 +1508,11 @@ class Kernel
      * Returns the session instance if no parameter passed.
      *
      *
-     * @param string $key
-     * @param mixed  $default
      *
+     *
+     * @param  string     $key
+     * @param  mixed      $default
      * @throws \Exception If $key not found and no $default passed.
-     *
      * @return mixed
      */
     public function session($key = null, $default = null)
@@ -1546,8 +1546,8 @@ class Kernel
     /**
      * Add a menu name to the history stack.
      *
-     * @param string $menuName
      *
+     * @param  string $menuName
      * @return void
      */
     protected function historyBagPush($menuName)
@@ -1594,13 +1594,13 @@ class Kernel
      * It returns the default connection if no connection name is provided.
      *
      *
-     * @param string $connectionName
      *
+     *
+     * @param  string     $connectionName
      * @throws \Exception If no it's unable to retrieve the configuration of the specified connection
      * @throws \Exception If no connectionNane passed and no default connection has been configured
      * @throws \Exception If connection to app database has not been activated in the application parameters (it is activated by default)
-     *
-     * @return \PDO The PDO connection
+     * @return \PDO       The PDO connection
      */
     public function db($connectionName = '')
     {
@@ -1714,7 +1714,7 @@ class Kernel
     public function network()
     {
         return $this->request->input(
-            $this->config('app.request_param_user_phone_number')
+            $this->config('app.request_param_user_network')
         );
     }
 
@@ -1761,8 +1761,8 @@ class Kernel
     /**
      * Retrieve the request input.
      *
-     * @param string $name
      *
+     * @param  string  $name
      * @return mixed
      */
     public function request($name = null)
@@ -1830,11 +1830,11 @@ class Kernel
      * The sender name and endpoint can be configure in the env file or
      * directly in the config/app.php file
      *
-     * @param string $message
-     * @param string $msisdn
-     * @param string $senderName
-     * @param string $endpoint
      *
+     * @param  string  $message
+     * @param  string  $msisdn
+     * @param  string  $senderName
+     * @param  string  $endpoint
      * @return void
      */
     public function sendSms($message, $msisdn = '', $senderName = '', $endpoint = '')
@@ -1851,10 +1851,10 @@ class Kernel
     /**
      * Return a path to a file or a folder.
      *
-     * @param string $key
      *
+     *
+     * @param  string               $key
      * @throws \RuntimeException
-     *
      * @return string
      */
     public function path($key = null)
