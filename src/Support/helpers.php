ea606b819168dfcb6b8d854bed664b9846cd2998
Prince Dorcis <prinx@users.noreply.github.com>
2020-11-10 10:52:42 +0000

    Apply fixes from StyleCI

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -4,7 +4,8 @@ if (!function_exists('project_root')) {
     /**
      * Project root directory.
      *
-     * @param  string   $append
+     * @param string $append
+     *
      * @return string
      */
     function project_root($append = '')
@@ -17,7 +18,8 @@ if (!function_exists('app_path')) {
     /**
      * Project app directory.
      *
-     * @param  string   $append
+     * @param string $append
+     *
      * @return string
      */
     function app_path($append = '')
@@ -30,7 +32,8 @@ if (!function_exists('config_path')) {
     /**
      * Project config directory.
      *
-     * @param  string   $append
+     * @param string $append
+     *
      * @return string
      */
     function config_path($append = '')
@@ -43,7 +46,8 @@ if (!function_exists('resource_path')) {
     /**
      * Project resource directory.
      *
-     * @param  string   $append
+     * @param string $append
+     *
      * @return string
      */
     function resource_path($append = '')
@@ -56,7 +60,8 @@ if (!function_exists('public_path')) {
     /**
      * Project public directory.
      *
-     * @param  string   $append
+     * @param string $append
+     *
      * @return string
      */
     function public_path($append = '')
@@ -69,7 +74,8 @@ if (!function_exists('storage_path')) {
     /**
      * Project storage directory.
      *
-     * @param  string   $append
+     * @param string $append
+     *
      * @return string
      */
     function storage_path($append = '')
@@ -82,7 +88,8 @@ if (!function_exists('test_path')) {
     /**
      * Project tests directory.
      *
-     * @param  string   $append
+     * @param string $append
+     *
      * @return string
      */
     function test_path($append = '')
@@ -95,7 +102,8 @@ if (!function_exists('is_associative')) {
     /**
      * Checks if an array is an associative array.
      *
-     * @param  array  $array
+     * @param array $array
+     *
      * @return bool
      */
     function is_associative(array $array = [])
