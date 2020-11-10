0a5c4adab00f39880faa08385b083f8ef70b077b
CinPer <damien.dorcis@yahoo.com>
2020-11-10 10:51:55 +0000

    Apply style

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -4,9 +4,7 @@ if (!function_exists('project_root')) {
     /**
      * Project root directory.
      *
-     *
-     * @param string $append
-     *
+     * @param  string   $append
      * @return string
      */
     function project_root($append = '')
@@ -19,9 +17,7 @@ if (!function_exists('app_path')) {
     /**
      * Project app directory.
      *
-     *
-     * @param string $append
-     *
+     * @param  string   $append
      * @return string
      */
     function app_path($append = '')
@@ -34,9 +30,7 @@ if (!function_exists('config_path')) {
     /**
      * Project config directory.
      *
-     *
-     * @param string $append
-     *
+     * @param  string   $append
      * @return string
      */
     function config_path($append = '')
@@ -49,9 +43,7 @@ if (!function_exists('resource_path')) {
     /**
      * Project resource directory.
      *
-     *
-     * @param string $append
-     *
+     * @param  string   $append
      * @return string
      */
     function resource_path($append = '')
@@ -64,9 +56,7 @@ if (!function_exists('public_path')) {
     /**
      * Project public directory.
      *
-     *
-     * @param string $append
-     *
+     * @param  string   $append
      * @return string
      */
     function public_path($append = '')
@@ -79,9 +69,7 @@ if (!function_exists('storage_path')) {
     /**
      * Project storage directory.
      *
-     *
-     * @param string $append
-     *
+     * @param  string   $append
      * @return string
      */
     function storage_path($append = '')
@@ -94,9 +82,7 @@ if (!function_exists('test_path')) {
     /**
      * Project tests directory.
      *
-     *
-     * @param string $append
-     *
+     * @param  string   $append
      * @return string
      */
     function test_path($append = '')
@@ -106,6 +92,12 @@ if (!function_exists('test_path')) {
 }
 
 if (!function_exists('is_associative')) {
+    /**
+     * Checks if an array is an associative array.
+     *
+     * @param  array  $array
+     * @return bool
+     */
     function is_associative(array $array = [])
     {
         foreach ($array as $key => $value) {
