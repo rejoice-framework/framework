4faf2dfc00603ebf2abd4b9275ead851f4d35bcd
Prince Dorcis <damien.dorcis@yahoo.com>
2021-04-16 16:54:16 +0000

    Add clear cache command

---
+++ src/Console/Commands/ClearCacheCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Rejoice\Console\Commands\FrameworkCommand as Smile;
+use Symfony\Component\Finder\Finder;
+
+class ClearCacheCommand extends Smile
+{
+    protected $defaultCacheFiles = ['log-count.cache', 'rejoice.cache'];
+
+    public function configure()
+    {
+        $this->setName('cache:clear')
+            ->setDescription('Clear the cache files.');
+    }
+
+    public function fire()
+    {
+        $finder = new Finder();
+
+        $finder->files()->ignoreDotFiles(false)->in(storage_path('cache'));
+
+        try {
+            foreach ($finder as $file) {
+                if (in_array($file->getFileName(), $this->defaultCacheFiles)) {
+                    file_put_contents($file->getPathname(), '');
+                } else {
+                    unlink($file->getPathname());
+                }
+            }
+        } catch (\Throwable $th) {
+            $this->error('Error!');
+            $this->write($th->getMessage().' in '.$th->getFile().' at line '.$th->getLine());
+
+            return Smile::FAILURE;
+        }
+
+        $this->info('Cache cleared successfully!');
+
+        return Smile::SUCCESS;
+    }
+}
