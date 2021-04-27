f6af4da1ff7da0e4d0f729cad752da990fe56ed3
Prince Dorcis <prinx@users.noreply.github.com>
2021-04-27 09:09:26 +0000

    Apply fixes from StyleCI

--- config/session.php
+++ config/session.php
@@ -16,13 +16,13 @@ return [
      * Timeout of the final response
      * Default 180 (3 minutes)
      */
-   'timeout' => 180,
+    'timeout' => 180,
 
     'database' => [
-        'user' => env('SESSION_DB_USER', ''),
+        'user'     => env('SESSION_DB_USER', ''),
         'password' => env('SESSION_DB_PASS', ''),
-        'host' => env('SESSION_DB_HOST', ''),
-        'port' => env('SESSION_DB_PORT', ''),
-        'dbname' => env('SESSION_DB_NAME', ''),
+        'host'     => env('SESSION_DB_HOST', ''),
+        'port'     => env('SESSION_DB_PORT', ''),
+        'dbname'   => env('SESSION_DB_NAME', ''),
     ],
 ];
